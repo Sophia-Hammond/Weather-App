b339bb6b70bf5322dce6163a2fb0143596e23d2d
Sophia-Hammond <beautyinbooty@gmail.com>
2025-04-03 23:26:27 +0100

    logic written for collecting weather data and error handeling

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const videoElement = document.getElementById("background-video");
     const videoSource = document.getElementById("video-source");
-
+    const weatherContainer = document.getElementById("weather.container");
+     
+    //API 
+    const API_KEY = "";
+    const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+    
+    
     //video source
     const dayVideo = "videos/daySky.mp4";
     const nightVideo = "videos/nightSky.mp4"
@@ -17,4 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     videoElement.load()
+
+    // Fetch weather data
+
+    async function fetchWeather(town) {
+        try {
+            const response = await fetch(`${BASE_URL}?q=${town}&appid=${API_KEY}&units=metric`);
+            if (!response.ok) throw new Error("Town not found");
+
+            const data = await response.json();
+            displayWeather(data);
+        } catch (error) {
+            console.error("Error fetching weather:", error);
+            weatherContainer.innerHTML = `<p>Error: ${error.message}</p>`;
+         }
+    }
+
+    // display fetched weather data
+    function displayWeather
 })
